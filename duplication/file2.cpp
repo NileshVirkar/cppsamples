b0e43a6005ddafced87112c50354b7cbdcde9e0d
Nilesh Virkar <nilesh@browserstack.com>
2024-09-03 17:04:22 +0530

    Update file2.cpp

--- duplication/file2.cpp
+++ duplication/file2.cpp
@@ -1,1 +1,35 @@
+// DerivedClass.hpp
+#ifndef DERIVED_CLASS_HPP
+#define DERIVED_CLASS_HPP
+
+#include "BaseClass.hpp"
+
+namespace teh {
+namespace types {
+
+class DerivedClass : public BaseClass {
+public:
+    double getStartAngle() const override {
+        return 45.0;
+    }
+
+    double getEndAngle() const override {
+        return 90.0;
+    }
+
+    bool isBridge() const override {
+        return true;
+    }
+
+    bool isTunnel() const override {
+        return true;
+    }
+
+    // Additional overrides...
+};
+
+} // namespace types
+} // namespace teh
+
+#endif // DERIVED_CLASS_HPP
 
