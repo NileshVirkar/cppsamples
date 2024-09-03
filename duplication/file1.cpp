1a6926b722f60ba1bf22360cc04f6918f27dfc1e
Nilesh Virkar <nilesh@browserstack.com>
2024-09-03 17:03:55 +0530

    Update file1.cpp

--- duplication/file1.cpp
+++ duplication/file1.cpp
@@ -1,1 +1,34 @@
+// BaseClass.hpp
+#ifndef BASE_CLASS_HPP
+#define BASE_CLASS_HPP
 
+namespace teh {
+namespace types {
+
+class BaseClass {
+public:
+    virtual ~BaseClass() = default;
+
+    virtual double getStartAngle() const {
+        return 0.0;
+    }
+
+    virtual double getEndAngle() const {
+        return 0.0;
+    }
+
+    virtual bool isBridge() const {
+        return false;
+    }
+
+    virtual bool isTunnel() const {
+        return false;
+    }
+
+    // Additional virtual functions...
+};
+
+} // namespace types
+} // namespace teh
+
+#endif // BASE_CLASS_HPP
