fbd50c3b9cc52ed5635824f580921f52aca08a71
Nilesh <nilesh.virkar@acellere.com>
2025-10-09 12:48:49 +0530

    csharp changes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World from .NET 9.0.305!");
+        // Example usage of the problematic class
+        using (var obj = new BadDisposeExample())
+        {
+            // ...
+        }
+    }
+}
+
+// CA1816: GC.SuppressFinalize should not be called unless the type has a finalizer
+public class BadDisposeExample : IDisposable
+{
+    public void Dispose()
+    {
+        // This is incorrect: GC.SuppressFinalize should only be called if the class has a finalizer
+        GC.SuppressFinalize(this); // CA1816 violation
     }
 }
